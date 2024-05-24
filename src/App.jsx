642af2cbd39110f7b92deb45054c1e040047ce6d
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-05-24 14:56:41 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,49 @@ import About from './pages/About.jsx';
 import Project from './pages/Project.jsx';
 import Contact from './pages/Contact.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import Logout from './pages/Logout.jsx';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState('');
 
   useEffect(() => {
-    // Check if the user is logged 
-    if (localStorage.getItem('token')) {
+    const token = localStorage.getItem('token');
+    console.log(token);
+    if (token) {
       setIsLoggedIn(true);
-      setEmail(localStorage.getItem('email'));
+      console.log("is logged in");
+      const storedEmail = localStorage.getItem('email');
+      if (storedEmail) {
+        setEmail(storedEmail);
+      }
     } else {
+      console.log('is not logged in');
       setIsLoggedIn(false);
+      setEmail('');
     }
+  
   }, []);
-
   return (
     <>
       <BrowserRouter>
-        {isLoggedIn}
+        {/* {isLoggedIn} */}
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="*" element={<NotFound />} />
           <Route path='/about' element={<About />} />
           <Route path='/project' element={<Project />} />
           <Route path='/contact' element={<Contact />} />
-          <Route path="/login" element={!isLoggedIn ? <Login setIsLoggedIn={setIsLoggedIn} setEmail={setEmail} /> : <Navigate to="/" />} />
-          <Route path="/dashboard" element={isLoggedIn ? <Dashboard email={email} setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/login" />} />
+          <Route path="/login" element={
+            !isLoggedIn ? 
+            <Login/> : 
+            <Navigate to="/" />} />
+          <Route path="/dashboard" element={
+            isLoggedIn ? 
+            <Dashboard email={email} /> :
+            <Navigate to="/login" /> 
+             } />
+          <Route path="/logout" element={<Logout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setEmail={setEmail}/>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/header/Header'
 
 function About() {
   return (
-    <div>
+    <>
+      <Header/>
       about
-    </div>
+    </>
   )
 }
 
