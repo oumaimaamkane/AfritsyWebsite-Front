1297fdd24c12707c086c8c9f76ceec9f83252232
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-05-24 14:54:04 +0100

    add api auth

--- src/API/Auth.js
+++ src/API/Auth.js
@@ -2,14 +2,13 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:8000/api";
 
-export function login(email, password) {
-    return axios.post(`${API_URL}/auth/login`, {
+export async function login(email, password) {
+    const response = await axios.post(`${API_URL}/auth/login`, {
         email,
         password
-    }).then(response => {
-        const { token, user } = response.data;
-        return { token, user }; 
     });
+    const { token, user } = response.data;
+    return { token, user };
 }
 
 export function logout(token) {
