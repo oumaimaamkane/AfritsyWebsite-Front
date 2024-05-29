06e9634527435d6531e86c03e6096ddf1d0df889
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-05-29 16:57:18 +0100

    add hero section in about page

---
+++ src/components/AboutPageSections/Benefices.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Benefices() {
+    return (
+
+        <div className="flex m-3 p-8 gap-12 flex-row items-start justify-center">
+
+            <div className="flex justify-center items-center p-4 "style={{ background: '#F0F0F6' }}>
+                <div className="flex flex-col items-center text-center">
+                    <div className="rounded-full p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFB600" className="w-14 h-14">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"></path>
+                        </svg>
+                    </div>
+                    <h3 className="text-3xl font-bold">Safe</h3>
+                    <p>Our products are secure and private out-of-the-box</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center p-4"style={{ background: '#F0F0F6' }}>
+                <div className="flex flex-col items-center text-center">
+                    <div className="rounded-full p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFB600" className="w-14 h-14">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"></path>
+                        </svg>
+                    </div>
+                    <h3 className="text-3xl font-bold">Safe</h3>
+                    <p>Our products are secure and private out-of-the-box</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center p-4"style={{ background: '#F0F0F6' }}>
+                <div className="flex flex-col items-center text-center">
+                    <div className="rounded-full p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFB600" className="w-14 h-14">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"></path>
+                        </svg>
+                    </div>
+                    <h3 className="text-3xl font-bold">Safe</h3>
+                    <p>Our products are secure and private out-of-the-box</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Benefices
+
+
+
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/Footer'
+import Hero from '../components/AboutPageSections/Hero'
+import Benefices from '../components/AboutPageSections/Benefices'
 
 function About() {
   return (
     <>
       <Header/>
-      about
+        <div className='carousel-container'>
+          <Hero />
+        </div>
+        <Benefices />
       <Footer />
     </>
   )
