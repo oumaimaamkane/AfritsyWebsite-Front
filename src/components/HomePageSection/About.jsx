2673dab7325c8ba6d63d824df612745c4dc07bc0
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-05-29 15:23:03 +0100

    create about section for home page

--- src/components/HomePageSection/About.jsx
+++ src/components/HomePageSection/About.jsx
@@ -3,46 +3,31 @@ import React from 'react';
 function About() {
   return (
     <>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex flex-col items-center">
+      <div className="font-sans text-[#333] max-w-6xl max-md:max-w-md mx-auto">
+        <div className="grid md:grid-cols-2 items-center md:gap-8 gap-6 m-3">
+          <div className="w-full lg:w-3/4 md:w-11/12 z-50 relative transform transition-transform duration-500 hover:scale-105 animate-fade-in" style={{ backgroundColor: '#FFB600' }}>
             <img
-              className="object-cover object-center rounded"
-              alt="Sample Hero"
               src="/images/front2.png"
-              style={{ position: 'absolute', top: '586px', left: '77px' }}
+              className="rounded-md object-cover md:mx-auto md:max-w-sm lg:max-w-none"
+              alt="Dining Experience"
+              style={{ margin: '30px' }}
             />
-            <img
-              className="object-cover object-center rounded"
-              alt="Second Image"
-              src="/images/back1.png"
-              style={{ position: 'absolute', top: '568px', left: '35px' }}
-            />
-            {/* <img
-              className="object-cover object-center rounded"
-              alt="Second Image"
-              src="/images/image3.png"
-            /> */}
           </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col items-center text-center md:text-left">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
-              Before they sold out
-              <br className="hidden lg:inline-block" /> readymade gluten
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.
-            </p>
-            <div className="flex justify-center lg:justify-start">
-              <a href="/project" className="inline-flex text-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg mb-4 lg:mb-0 lg:mr-4" style={{ background: '#FFB600' }}>
-                Discover Our Project
-              </a>
-              <a href="/about" className="inline-flex text-black bg-white border-2 border-black py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                ABOUT US
-              </a>
+
+          <div className="max-md:order-1 max-md:text-center animate-slide-in-left">
+            <h2 className="md:text-5xl text-3xl font-extrabold mb-4 md:!leading-[55px] text-center">Schedule meetings effortlessly</h2>
+            <p className="mt-4 text-base text-gray-500 leading-relaxed text-center">Embark on a gastronomic journey with our curated dishes, delivered promptly to your doorstep. Elevate your dining experience today.</p>
+
+            <div className="mt-10 space-x-4 text-center animate-slide-in-left">
+              <button type='button'
+                className="inline-flex text-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 text-lg mb-4 lg:mb-0 lg:mr-4 transition duration-300 ease-in-out transform hover:scale-110" style={{ background: '#FFB600' }}>Discover Our Project</button>
+              <button type='button'
+                className="bg-transparent hover:bg-yellow-400 hover:text-white border-2 border-black transition-all text-black font-bold text-sm px-6 py-2.5 transition duration-300 ease-in-out">ABOUT US</button>
             </div>
+            <hr className="mt-10" />
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 }
