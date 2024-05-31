987fd217d597817005d2e8fec1c36be308afabfc
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-05-31 09:59:11 +0100

    add package for animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
