a64594ef6fac9dda522999cec36e8654d1cc1216
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-06-10 14:00:49 +0100

    create section story in about page

--- src/components/AboutPageSections/Story.jsx
+++ src/components/AboutPageSections/Story.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
 function Story() {
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+
     return (
         <div className="mx-auto">
             <div className="text-center p-8">
@@ -11,11 +17,11 @@ function Story() {
 
                     <div className="xl:w-[80%] sm:w-[85%] xs:w-[90%] mx-auto flex flex-col md:flex-row xs:flex-col lg:gap-4 xs:gap-2 justify-center lg:items-stretch md:items-center mt-4">
                         <div className="lg:w-[50%] xs:w-full animate__animated animate__fadeIn">
-                            <img className="rounded-lg" src="/images/Frame 35.png" alt="billboard image" />
+                            <img className={`rounded-lg transition-all duration-700 ease-in-out ${inView ? 'opacity-100 transform translate-y-0' : 'opacity-0 translate-y-10'}`} src="/images/Frame 35.png" alt="billboard image" ref={ref} />
                         </div>
                         <div className="lg:w-[50%] sm:w-full xs:w-full md:p-4 xs:p-0 rounded-md animate__animated animate__fadeIn">
-                            <h2 className="text-3xl font-semibold text-gray-900 ">Lorem ipsum dolor sit amet consectetur</h2>
-                            <p className="text-md mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam
+                            <h2 className="text-3xl font-semibold text-gray-900 transition-all duration-300 transform">{inView ? "Lorem ipsum dolor sit amet consectetur" : ""}</h2>
+                            <p className={`text-md mt-4 transition-all duration-300 transform ${inView ? 'opacity-100' : 'opacity-0'}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam
                                 veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi
                                 sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga
                                 nobis tempora possimus ullam!</p>
@@ -26,20 +32,19 @@ function Story() {
 
 
                         <div className="lg:w-[50%] xs:w-full md:p-4 xs:p-0 rounded-md animate__animated animate__fadeIn">
-                            <h2 className="text-3xl font-semibold text-black ">Lorem ipsum dolor sit amet consectetur</h2>
+                            <h2 className="text-3xl font-semibold text-black transition-all duration-300 transform">{inView ? "Lorem ipsum dolor sit amet consectetur" : ""}</h2>
 
-                            <p className="text-md mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam
+                            <p className={`text-md mt-4 transition-all duration-300 transform ${inView ? 'opacity-100' : 'opacity-0'}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore placeat assumenda nam
                                 veritatis, magni doloremque pariatur quos fugit ipsa id voluptatibus deleniti officiis cum ratione eligendi
                                 sed necessitatibus aliquam error laborum delectus quaerat. Delectus hic error eligendi sed repellat natus fuga
                                 nobis tempora possimus ullam!</p>
                         </div>
 
                         <div className="md:block sm:hidden xs:hidden lg:w-[50%] xs:w-full animate__animated animate__fadeIn">
-                            <img className="rounded-lg" src="/images/pana.png" alt="billboard image" />
+                            <img className={`rounded-lg transition-all duration-700 ease-in-out ${inView ? 'opacity-100 transform translate-y-0' : 'opacity-0 translate-y-10'}`} src="/images/pana.png" alt="billboard image" ref={ref} />
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
