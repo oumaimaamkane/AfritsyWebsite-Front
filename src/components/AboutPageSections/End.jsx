bf0f9ba95472451ec964ae4df33ca5a6990562a5
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-06-10 14:00:26 +0100

    create section end in about page

--- src/components/AboutPageSections/End.jsx
+++ src/components/AboutPageSections/End.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 function End() {
     return (
-        <div className="text-black px-8 py-8 w-full rounded-lg font-[sans-serif] flex flex-col items-center">
-            <h1 className="text-4xl font-extrabold">Hello World!</h1>
-            <p className="mt-4 text-sm text-black text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
-                tempus blandit, metus mi consectetur nibh, a pharetra felis turpis vitae ligula. Etiam laoreet velit nec neque
-                ultrices, non consequat mauris tincidunt.</p>
-            <button type="button"
-                className="px-6 py-3 m-3 text-base font-semibold text-white bg-[#FFB600] rounded-full hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105 focus:ring-2 focus:ring-[#f032e6] focus:outline-none focus:ring-opacity-50">Learn
-                more</button>
-        </div>
+        <>
+            <div class="bg-gradient-to-r from-[#EFA300] to-[#242526] py-20 px-6 font-[sans-serif]">
+                <div class="container mx-auto text-center">
+                    <h2 class="text-4xl font-extrabold text-white mb-6">Join Us Today</h2>
+                    <p class="text-lg text-white mb-8">Experience the future of our innovative solutions. Start now for parfect project.</p>
+                    <a href="/project" class="bg-[#FFB600] text-white hover:bg-white hover:text-[#FFB600] py-3 px-8 rounded-full text-lg font-semibold transition duration-300 hover:shadow-lg">
+                        OUR PROJECTS
+                    </a>
+                </div>
+            </div>
+        </>
     )
 }
 
