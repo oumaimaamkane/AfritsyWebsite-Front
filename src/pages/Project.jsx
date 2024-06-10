3dd98b0f9441faddc2a7c0335d3940a5cd817d96
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-06-10 14:06:00 +0100

    modification in style and link to components in project page

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Header from '../components/header/Header'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Hero from '../components/ProjectsPageSections/Hero'
+import Projects from '../components/ProjectsPageSections/Projects'
 
 function Project() {
   return (
     <>
-      <Header/>
-      project
+      <Header />
+          <div className="carousel-container">
+            <Hero />
+          </div> 
+          <Projects />
       <Footer />
+      
     </>
   )
 }
