ea4a7a85220a4b3bd91987f0f5851f43459d2fd5
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-06-10 13:59:44 +0100

    create contact form the send email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import Header from '../components/header/Header'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Map from '../components/ContactPageSections/Map'
+import backgroundImage from '../assets/afritsy.jpg';
+import ContactUs from '../components/ContactPageSections/ContactUs'
 
 function Contact() {
   return (
     <div>
       <Header/>
-      contact
+      
+          <div className="carousel-container">
+            <ContactUs />
+          </div>
+            <Map/>
+
+          
       <Footer />
     </div>
   )
