48b28e53f6cc55d23db8fc0a20a1bab7f4a1d978
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-06-10 14:05:14 +0100

    modify link to componenets in home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header/Header'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Hero from '../components/AboutPageSections/Hero'
 import Benefices from '../components/AboutPageSections/Benefices'
